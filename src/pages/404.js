f14183822c88f727301d785d5e22a51211fcb29e
Rus7Iv <rusivary@gmail.com>
2023-05-17 19:43:49 +0400

    Добавлена страницы 404

---
+++ src/pages/404.js
@@ -0,0 +1,44 @@
+import Footer from "@/components/Footer";
+import Navigation from "@/components/Navigation";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Custom404() {
+  const router = useRouter();
+
+  return (
+    <>
+      <Navigation />
+      <main>
+        <div
+          style={{
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <h1 style={{ fontWeight: "bold" }}>404 - Страница не найдена</h1>
+          <p style={{ width: "100%", textAlign: "center", marginTop: "30px" }}>
+            Кажется, вы попали на страницу, которой не существует. Пожалуйста,
+            вернитесь на{" "}
+            <Link
+              href="/"
+              style={{
+                color: "#555",
+                textDecoration: "none",
+                fontWeight: "bold",
+              }}
+            >
+              {" "}
+              главную страницу
+            </Link>
+            .
+          </p>
+          <button onClick={() => router.back()} className="btns">
+            Назад
+          </button>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
