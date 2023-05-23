dc8bad0451effabf723dd9f6238b3bfcf5587563
Rus7Iv <rusivary@gmail.com>
2023-05-23 18:44:51 +0400

    Окончена работа над Promo

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -1,73 +1,3 @@
-// import React from "react";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import styles from "../styles/Promo.module.css";
-// import Link from "next/link";
-// import Image from "next/image";
-// import arrowLeft from "../../public/arrow-left.svg";
-// import arrowRight from "../../public/arrow-right.svg";
-
-// const Promo = ({ images }) => {
-// const CustomPrevArrow = (props) => {
-//   const { className, style, onClick } = props;
-//   return (
-//     <div className={className} style={{ ...style, display: "block" }}>
-//       <Image
-//         src={arrowLeft}
-//         alt="Prev"
-//         width={24}
-//         height={24}
-//         onClick={onClick}
-//       />
-//     </div>
-//   );
-// };
-
-// const CustomNextArrow = (props) => {
-//   const { className, style, onClick } = props;
-//   return (
-//     <div className={className} style={{ ...style, display: "block" }}>
-//       <Image
-//         src={arrowRight}
-//         alt="Next"
-//         width={24}
-//         height={24}
-//         onClick={onClick}
-//       />
-//     </div>
-//   );
-// };
-
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     prevArrow: <CustomPrevArrow />,
-//     nextArrow: <CustomNextArrow />,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-//   };
-
-//   return (
-//     <div className={styles.wrapper}>
-//       <Slider {...settings} className={styles.carousel}>
-//         {images.map((image, index) => (
-//           <div key={index}>
-//             <Link href={image.link}>
-//               <img src={image.src} alt={image.alt} className={styles.image} />
-//             </Link>
-//           </div>
-//         ))}
-//       </Slider>
-//     </div>
-//   );
-// };
-
-// export default Promo;
-
 import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -134,8 +64,8 @@ const Promo = () => {
     slidesToScroll: 1,
     prevArrow: <CustomPrevArrow />,
     nextArrow: <CustomNextArrow />,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
   };
 
   return (
@@ -147,10 +77,12 @@ const Promo = () => {
               <img
                 src={product.image[0]}
                 alt={product.title}
-                // width={500}
-                // height={420}
-                style={{ width: "100%" }}
+                className={styles.img_}
               />
+              <div className={styles.details}>
+                <p className={styles.name}>{product.title}</p>
+                <p className={styles.price}>{product.price} ₽</p>
+              </div>
             </Link>
           </div>
         ))}
